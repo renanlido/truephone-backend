11f4564a93db50290f14fd505aebb22c305e15a5
renanlido <renanlido@yahoo.com.br>
2022-02-14 10:50:32 -0300

    feat: add get route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,5 +6,13 @@ const routes = Router();
 const createFileController = new CreateFileController();
 
 routes.post("/files/create", createFileController.handle);
+routes.get("/", (request, response) => {
+  response.json({
+    message: "Parabéns, vc chegou!",
+    routeSpecs: {
+      disponibleRoutes: ["/file/create"],
+    },
+  });
+});
 
 export { routes };
